85115b8b62fdb65858ebfb60f61b4fb2a78d3a72
Goufalite <36901323+Goufalite@users.noreply.github.com>
2022-12-28 19:41:33 +0100

    Plus d'infos sur les reminders

--- reminder.php
+++ reminder.php
@@ -5,6 +5,104 @@ header("Content-type:text/html;CHARSET=UTF-8");
 include "fonctions/config.inc.php";
 include "fonctions/connector.php";
 
+function parseUrl($txt, $key)
+{
+	$matches = array();
+	$filtersreg = preg_match("@^(.*?)/stops_schedules\.json(?:.*?)stopsList=(.*?)$@",$txt,$matches);
+	$filters = $matches[2];
+	$filtertab = explode(",",$filters);
+	foreach($filtertab as $filter)
+	{
+		$criterias = explode("|",$filter);
+		if (preg_match("/SA/",$criterias[0]))
+		{
+			//SA
+			$elt = json_decode(file_get_contents($matches[1]."/stop_areas/".$criterias[0].".json?lineId=".$criterias[1].(isset($criterias[2])?"&terminusId=".$criterias[2]:"")."&displayLines=1&key=$key"));
+			$mystation = $elt->stopAreas->stopArea[0];
+			foreach($mystation->line as $line)
+			{
+				if ($line->id == $criterias[1])
+				{
+					$lineres = $line;
+					break;
+				}
+			}
+			$line = "<div class='busline' style='display:inline;background-color:rgb".$lineres->color."'>".$lineres->shortName."</div>";
+
+			// l'api ne renvoit pas le terminus même avec tous les critères
+			$terminus = "??";
+			if (isset($criterias[2]))
+			{
+				$elt2 = json_decode(file_get_contents($matches[1]."/stop_areas/".$criterias[2].".json?key=$key"));
+				$terminus = $elt2->stopAreas->stopArea[0]->name;
+			}
+			$stationname = $mystation->name;
+		}
+		else
+		{
+			$res = explode('|',parseStopPoint($matches[1],$criterias,$key));
+			$line = $res[0];
+			$terminus = $res[1];
+			$stationname = $res[2];
+			
+		}
+		
+		echo $line."&nbsp;".$stationname." --&gt; ".$terminus."<br/>\n";
+	}
+	echo "<br/>\n";
+}
+
+function parseStopPoint($url, $criterias, $key)
+{
+	$elt = json_decode(file_get_contents($url."/stop_points/".$criterias[0].".json?displayLines=1&key=$key"));
+	$mystation = $elt->physicalStops->physicalStop[0];	
+	$lines = $mystation->destinations;
+	if (count($lines) == 1)
+	{
+		// une seule ligne
+		$line = "<div class='busline' style='display:inline;background-color:rgb".$lines[0]->line[0]->color."'>".$lines[0]->line[0]->shortName."</div>";
+		$terminus = $lines[0]->name;
+	}
+	else
+	{
+		// plusieurs destination pour cet arrêt, donc filtre
+		if (isset($criterias[1]))
+		{
+			// filtre par ligne
+			foreach($lines as $dest)
+			{
+				if ($dest->line[0]->id == $criterias[1])
+				{
+					$line = "<div class='busline' style='display:inline;background-color:rgb".$dest->line[0]->color."'>".$dest->line[0]->shortName."</div>";
+					$terminus = "??";
+					if (isset($criterias[2]))
+					{
+						// filtre par terminus
+						if ($dest->id == $criterias[2])
+						{
+							$terminus = $dest->name;
+						}
+						break;
+					}
+					else
+					{
+						// on prend le premier correspondant à la ligne
+						$terminus = $dest->name;
+						break;
+					}					
+				}
+			}
+		}
+		else
+		{
+			// plusieurs destinations et on ne filtre pas : inconnu!
+			$line = "<div class='busline' style='display:inline;background-color:rgb(160,103,15)'>??</div>";
+			$terminus = "??";
+		}
+	}
+	return "$line|$terminus|".$mystation->name;
+}
+
 try{
 	$connec = new SQLiteConnector();
 	if ($connec->connect(DB,SQLITE3_OPEN_READWRITE))
@@ -112,6 +210,11 @@ function loaded()
 {
 	
 }
+
+function details()
+{
+	document.getElementById("details").style.display = "block";
+}
 </script>
 <style type='text/css'>
 
@@ -185,7 +288,7 @@ foreach($json->departures->departure as $d)
 		if ($first)
 		{
 			// estimation de départ
-			echo "<div id='departOptimal'>D&eacute;part optimal dans ".intval(($ecart-$offset)/60)." minutes </div><br/>";
+			echo "<div id='departOptimal' onclick='details();'>D&eacute;part optimal dans ".intval(($ecart-$offset)/60)." minutes </div><br/>";
 		
 		}
 		$arr[] =  "<tr><td class='busline' style='background-color:rgb".$d->line->color."'>".$d->line->shortName."</td><td>".substr(preg_split("@ @",$d->dateTime)[1],0,5)." -&gt; ".intval($ecart/60)." min".($d->realTime==="no"?"*":"")."</td></tr>\n";
@@ -197,6 +300,9 @@ foreach($json->departures->departure as $d)
 		$arr[] =  "<tr><td class='busline' style='background-color:rgb".$d->line->color."'>".$d->line->shortName."</td><td class='before'>".substr(preg_split("@ @",$d->dateTime)[1],0,5)." -&gt; ".intval($ecart/60)." min".($d->realTime==="no"?"*":"")."</td></tr>\n";
 	}
 }
+echo "<div id='details' style='display:block;'>";
+parseUrl($currloc["url"],$key);
+echo "</div>";
 echo "<div id='linesResult'><table>";
 foreach($arr as $s)
 {
